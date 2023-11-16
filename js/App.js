a9efc227e5a98f503ef0e378295f25cf6369c7c5
hsyeon4001 <hsyeon4001@gmail.com>
2023-11-16 18:19:22 +0900

    🎉 Initialize a project

---
+++ js/App.js
@@ -0,0 +1,27 @@
+export default class App {
+  static canvas = document.querySelector('canvas');
+  static ctx = App.canvas.getContext('2d');
+  static dpr = devicePixelRatio > 1 ? 2 : 1;
+
+  constructor() {}
+
+  init() {
+    App.canvas.width = App.width * App.dpr;
+    App.canvas.height = App.height * App.dpr;
+    App.ctx.scale(App.dpr, App.dpr);
+  }
+  render() {
+    let now, delta;
+    let then = Date.now();
+
+    const frame = () => {
+      requestAnimationFrame(frame);
+      now = Date.now();
+      delta = now - then;
+
+      then = now - (delta % App.interval);
+    };
+
+    requestAnimationFrame(frame);
+  }
+}
